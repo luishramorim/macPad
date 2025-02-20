f106f54329bc67d798be2107cb1c32f432178970
Luis Amorim <luis@amorim.bio.br>
2025-02-20 09:26:24 -0300

    Initial Commit

---
+++ macPad/macPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  macPadApp.swift
+//  macPad
+//
+//  Created by Luis Amorim on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct macPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
